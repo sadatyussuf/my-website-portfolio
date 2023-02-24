c174ff0b7fc389ae6cfaafa1ec1fd07c1e6a1d8a
sadatyussuf <tommyleespartan0@gmail.com>
2023-02-24 22:52:12 +0000

    added the Navbar component to App.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
-
+import Navbar from "./components/Navbar"
 function App() {
 
 
   return (
-    <div className="w-0 bg-gray-700">
-     <h1 className="text-3xl font-bold underline text-red-600">Hi</h1>
-    </div>
+<>
+<Navbar/>
+</>
   )
 }
 
