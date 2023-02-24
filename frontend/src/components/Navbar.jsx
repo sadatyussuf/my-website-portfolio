55d710c58d0ce7e9f73b089b738578ca0225cf13
sadatyussuf <tommyleespartan0@gmail.com>
2023-02-24 22:54:26 +0000

    started with laying out the bare-bones structure of my Navbar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const Navbar = () => {
+
+    return (
+    <nav className='flex min-h-screen flex-col text-white'>
+
+        <div className='flex'>
+            <img src="" alt="" />
+            <p>portfolio</p>
+        </div>
+        <div className="flex">
+            {/* <ul className='flex'>
+                <li>lorem</li>
+                <li>ipsum</li>
+                <li>dolor</li>
+                <li>sitzum</li>
+            </ul> */}
+        </div>
+
+        <div className='flex'>
+            {/* <ul>
+                <li>Github</li>
+                <li>LinkedIn</li>
+                <li>Twitter</li>
+            </ul> */}
+        </div>
+    </nav>
+    )
+}
+
+export default Navbar
