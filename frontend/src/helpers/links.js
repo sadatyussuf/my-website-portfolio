0fc7f523c6af77c02cdfa566d519d60cd963b0c8
sadatyussuf <tommyleespartan0@gmail.com>
2023-02-24 22:53:20 +0000

    created a file to store my links

---
+++ frontend/src/helpers/links.js
@@ -0,0 +1,49 @@
+
+
+export const links = [
+    // {
+    //     "id": 1,
+    //     "name": "resume",
+    //     "url": "/"
+    // },
+    {
+        "id": 1,
+        "name": "about",
+        "url": "/about"
+    },
+    {
+        "id": 2,
+        "name": "technologies",
+        "url": "/technologies"
+    },
+    {
+        "id": 3,
+        "name": "contact",
+        "url": "/contact"
+    },
+    {
+        "id": 4,
+        "name": "profile",
+        "url": "/profile"
+    },
+]
+
+
+export const socials = [
+    {
+        "id": 1,
+        "url": "https://github.com/sadatyussuf",
+        // "icon": <FaFacebook />
+    },
+    {
+        "id": 2,
+        "url": "https://twitter.com/yussufSadat",
+        // "icon": <FaTwitter />
+    },
+    {
+        "id": 3,
+        "url": "https://www.linkedin.com/in/anwur-sadat-yussuf-issah-aa48b11b2/",
+        // "icon": <FaLinkedin />
+    },
+
+]
