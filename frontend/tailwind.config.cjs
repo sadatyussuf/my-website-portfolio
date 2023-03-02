00b277a1b347cfec5e07fe383e93ed4c6b9bb91e
sadatyussuf <tommyleespartan0@gmail.com>
2023-03-02 22:58:32 +0000

    added screen sizes for my media query and website colors

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -3,7 +3,21 @@ module.exports = {
   content: ["./index.html",
     "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        deepBlack: '#0B0C10',
+        deepGrey: '#1F2833',
+        lightGrey: '#C5C6C7',
+        lightBlue: '#66FCF1',
+        greyishBlue: '#45A29E'
+      }
+    },
   },
   plugins: [],
 }
