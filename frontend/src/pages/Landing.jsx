d4f4bf5a059935339b19804afb413205b9ac37ec
sadatyussuf <tommyleespartan0@gmail.com>
2023-03-07 21:34:18 +0000

    added Navbar and Hero component to Landing component

---
+++ frontend/src/pages/Landing.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from "../components/Navbar"
+import Hero from "../components/Hero"
+
+const Landing = () => {
+  return (
+    <main>
+      <Navbar/>
+      <Hero/>
+    </main>
+  )
+}
+
+export default Landing
