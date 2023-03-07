5cd395148081ccf558505b572f852096efd352b8
sadatyussuf <tommyleespartan0@gmail.com>
2023-03-07 21:34:51 +0000

    created a WelcomeMsg component

---
+++ frontend/src/components/WelcomeMsg.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Welcome = () => {
+  return (
+    <div className='flex flex-col gap-5'> 
+        <span className='font-Rampart text-2xl tracking-wider text-lightBlue'>Hi,👋</span>
+        <span className='font-Rampart text-2xl tracking-wider text-lightBlue'>Welcome To My Personal Portfolio!</span>
+        <span className='text-greyishBlue'>My Name is <span className='capitalize text-lg text-lightBlue'>anwur sadat yussuf issah</span> ,I am a software developer and geospatial analyst, and I am passionate about using technology to solve real-world problems</span>
+    </div>
+  )
+}
+
+export default Welcome
