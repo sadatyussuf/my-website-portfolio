f1e73ea3b607b176d01e2e2796c30c0591d5fbac
sadatyussuf <tommyleespartan0@gmail.com>
2023-04-01 19:03:08 +0000

    added ProfileImage component and added new tailwind styles

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Welcome from './WelcomeMsg'
+import ProfileImage from './ProfileImage'
 
 const Hero = () => {
   return (
-    <section className='flex flex-col md:flex-row '>
+      <section className='flex flex-col md:flex-row items-center mx-auto px-6 h-[90vh]' >
         <Welcome/>
-        <div>Picture</div>
+        <ProfileImage/>
 
     </section>
   )
