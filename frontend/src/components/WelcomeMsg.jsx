b407a2756a3852531064c2bedc9dee0bd02c7e9e
sadatyussuf <tommyleespartan0@gmail.com>
2023-04-01 19:06:58 +0000

    created a component for the Hero Welcome Message

--- frontend/src/components/WelcomeMsg.jsx
+++ frontend/src/components/WelcomeMsg.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 
 const Welcome = () => {
   return (
-    <div className='flex flex-col gap-5'> 
-        <span className='font-Rampart text-2xl tracking-wider text-lightBlue'>Hi,👋</span>
-        <span className='font-Rampart text-2xl tracking-wider text-lightBlue'>Welcome To My Personal Portfolio!</span>
-        <span className='text-greyishBlue'>My Name is <span className='capitalize text-lg text-lightBlue'>anwur sadat yussuf issah</span> ,I am a software developer and geospatial analyst, and I am passionate about using technology to solve real-world problems</span>
+    // mb-32
+    <div className='flex flex-col  space-y-12 md:w-1/2'> 
+        <span className='max-w-md font-Rampart text-4xl font-bold tracking-wider text-lightBlue text-center md:text-6xl md:text-left'>Hi,👋</span>
+
+        <span className=' max-w-lg font-Rampart text-4xl md:text-6xl text-lightBlue text-center md:text-left'>Welcome To My Personal Portfolio!</span>
+
+        <span className='max-w-lg text-center text-greyishBlue md:text-left md:text-2xl'>My Name is <span className='capitalize text-lg text-lightBlue md:text-3xl'>anwur sadat yussuf issah</span> ,I am a software developer and geospatial analyst, and I am passionate about using technology to solve real-world problems.</span>
+
     </div>
   )
 }
