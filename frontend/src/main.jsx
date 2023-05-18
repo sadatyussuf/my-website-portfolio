810b3b6dcc0e148fb3b95378ba83c65f7f18b762
sadatyussuf <tommyleespartan0@gmail.com>
2023-05-18 22:19:43 +0000

    added Router to main file, routing to root page,error page and about page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,36 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import ErrorPage from "./pages/Error"
+import AboutPage from "./pages/About"
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<App /> ,
+    errorElement: <ErrorPage /> ,
+//     children: [
+// {
+//     path: "about/",
+//     element: <AboutPage />,
+//   },
+//     ]
+  },
+
+  {
+    path: "about/",
+    element: <AboutPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+   <RouterProvider router={router}/>
+    {/* <App /> */}
   </React.StrictMode>,
 )
