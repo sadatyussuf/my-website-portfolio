45727a6a7d945357b634fabf66ead8812f5d0ac3
sadatyussuf <tommyleespartan0@gmail.com>
2023-05-18 22:22:53 +0000

    content for the about page

---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Header from '../components/AboutHeader'
+const  About = () => {
+  return (
+   
+    <main className='h-screen'>
+        <Navbar/>
+        <Header/>
+    </main>
+  )
+}
+
+export default  About
