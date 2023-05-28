b362498a5e841f3c1078c613ac06303210fcd0d4
sadatyussuf <tommyleespartan0@gmail.com>
2023-05-28 19:44:57 +0000

    creating AboutHeader component

--- frontend/src/components/AboutHeader.jsx
+++ frontend/src/components/AboutHeader.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
-const AboutHeader = () => {
+const AboutHeader = ({props}) => {
+  console.log(props)
   return (
-    <div>AboutHeader</div>
+    <section className='flex flex-col border-2 border-red-500 rounded-md w-full'>
+      <p>Date</p>
+      <h3>Place Title</h3>
+      <p>Place Name</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim in ea rerum consequatur placeat harum consectetur ad omnis illum delectus veniam exercitationem est eveniet aperiam, commodi nostrum. Sunt, ab harum.</p>
+    </section>
   )
 }
 
